9a3187a40db599225cf360d322991a8f48fd6bc2
Carlos Enmanuel Soto Lopez <72246303+carlossoto362@users.noreply.github.com>
2022-06-05 20:55:55 +0200

    Add files via upload
    Three data sets with names for different sectors. The challenge was to Analise the underlain reasons why the distribution of names didn't look random on each sector of research.   For Italy, the gender inequality and the region were prevalent reasons.

---
+++ challenge2/analisis.py
@@ -0,0 +1,22 @@
+#us only public universities
+#In what sence, this are not only random distribution on names. Potential reasons why this people are not randomly distributed on the departments. 
+
+
+
+import csv
+import pandas as pd
+
+df = pd.read_csv('france_clean.csv')
+
+print(df[df['city'] == df['city'][0]])
+
+
+
+
+
+				
+
+	
+
+
+
